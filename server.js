3cb924b36bd3456405643fe8cb11e94344cd823f
Naomi Thomson <naomithomson94@gmail.com>
2017-07-21 14:25:26 -0700

    added get user by id

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // library imports
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 // local imports
 var {mongoose} = require('./db/mongoose');
@@ -25,7 +26,33 @@ app.post('/users', (req, res) => {
 
 // returns users
 app.get('/users', (req, res) => {
+  User.find().then((users) => {
+    res.send({users})
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+// returns specific user
+app.get('/users/:id', (req, res) => {
+  var id = req.params.id;
   
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }; 
+
+  User.findById(id).then((user) => {
+    if(!user) {
+      return res.status(404).send();
+    };
+
+    res.send({user});
+
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+
 })
 
 app.listen(3000, () => {
